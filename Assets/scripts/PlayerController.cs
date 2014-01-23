ea6c52aadc778c9f167cd75a701606e0d4fe230e
Alex Zabolotny <alexander.zabolotny@gmail.com>
2014-01-22 22:13:06 -0500

    Initial commit

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public const string PICKUP_TAG = "Pickup";
+
+	public float speed = 100;
+
+	private int collected = 0;
+	private int total	  = 0;
+
+	public GUIText countText;
+	public GUIText winText;
+
+	public GameObject firework;
+
+	void Start() {
+		SetCountText ();
+		winText.text = "";
+		firework.SetActive (false);
+
+		GameObject[] pickups = GameObject.FindGameObjectsWithTag (PICKUP_TAG);
+		total = pickups.Length;
+	}
+
+	void FixedUpdate() {
+		float horizontal = Input.GetAxis ("Horizontal");
+		float vertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (horizontal, 0.0f, vertical);
+
+		rigidbody.AddForce (movement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == PICKUP_TAG) {
+			other.gameObject.SetActive (false);
+			collected++;
+			SetCountText();
+		}
+	}
+
+	void SetCountText() {
+		countText.text = "Count: " + collected.ToString ();
+
+		if (collected >= total && total > 0) {
+			winText.text = "You win!";
+			firework.SetActive (true);
+		}
+	}
+}
